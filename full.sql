940071382a3d1fcb2abf9b208a1eb9a246c80d32
screamcol <screamcol@gmail.com>
2020-07-15 15:43:45 +0300

    Home task for lesson_5. NOT FINISHED

--- full.sql
+++ full.sql
@@ -1,43 +1,27 @@
 BEGIN;
 
-DROP TABLE IF EXISTS customers CASCADE;
-CREATE TABLE customers (id bigserial PRIMARY KEY, name VARCHAR(255));
-INSERT INTO customers (name) VALUES
-('Petr'),
-('Vasiliy'),
-('Mitrofan'),
-('Ivan'),
-('Alexey'),
-('Evgeney');
-
 DROP TABLE IF EXISTS products CASCADE;
 CREATE TABLE products (id bigserial PRIMARY KEY, title VARCHAR(255), price INTEGER);
 INSERT INTO products (title, price) VALUES
 ('t-shirt', 10),
-('hat', 5),
+('hat', 3),
 ('lamp', 50),
 ('notebook', 3),
-('laptop', 899),
+('laptop', 999),
 ('book', 25),
 ('TV', 500),
 ('shorts', 15),
-('panties', 10);
-
-DROP TABLE IF EXISTS order_items CASCADE;
-CREATE TABLE order_items (customer_id bigint, product_id bigint, FOREIGN KEY (customer_id) REFERENCES customers(id), FOREIGN KEY (product_id) REFERENCES products(id));
-INSERT INTO order_items (customer_id, product_id) VALUES
-(6, 5),
-(6, 7),
-(6, 3),
-(1, 1),
-(1, 2),
-(3, 7),
-(4, 3),
-(5, 8),
-(5, 9),
-(2, 9),
-(2, 8),
-(2, 6),
-(2, 2);
+('table', 200),
+('axe', 98),
+('hammer', 80),
+('chainsaw', 299),
+('baseball cap', 13),
+('sandals', 21),
+('pants', 30),
+('jacket', 37),
+('scooter', 999),
+('jeans', 10),
+('mivroeave owen', 236),
+('iron', 79);
 
 COMMIT;
